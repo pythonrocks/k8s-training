3a98672ad3e6ea7f139cb9a4dd093ca67b9fd043
Mykola Krysiuk <mkrysiuk@lohika.com>
2020-04-14 21:59:48 +0300

    update to session #3 homework

--- app/main.py
+++ app/main.py
@@ -1,8 +1,89 @@
-from fastapi import FastAPI
+import os
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 app = FastAPI()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+
+Base.metadata.create_all(engine)
+
+
+class User(BaseModel):
+    id: int
+    email: str
+    password: str
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(UserModel).filter(UserModel.email == email).first()
+
+
+def get_users(db: Session):
+    return db.query(UserModel).all()
+
+
+def create_user(db: Session, user: UserCreate):
+    fake_hashed_password = user.password + "just_ignoring_this_for_now"
+    db_user = UserModel(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/users/", response_model=User)
+def add_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="User already registered")
+    return create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=List[User])
+def get_users(db: Session = Depends(get_db)):
+    return get_users(db)
